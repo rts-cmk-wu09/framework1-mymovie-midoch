31a5779c5551cb1109f169129ac805372a7c02a9
mido <101448145+midoch@users.noreply.github.com>
2023-04-20 14:13:48 +0200

    Create MovieRating.jsx

---
+++ src/components/MovieRating.jsx
@@ -0,0 +1,12 @@
+import { FaStar } from "react-icons/fa";
+
+const MovieRating = () => {
+  return (
+    <div>
+      <FaStar />
+      <p>9.1/10 IMBDb</p>
+    </div>
+  );
+};
+
+export default MovieRating;
